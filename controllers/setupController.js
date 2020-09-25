4b57adc87d6b7b5b174de0cfcbb45711323ee104
unknown <mohamed.amine.dichkour@gmail.com>
2020-09-25 16:40:38 +0100

    Everything saved to DB

--- controllers/setupController.js
+++ controllers/setupController.js
@@ -1,4 +1,5 @@
 var connection  = require('../lib/db');
+var setupModel  = require('../models/setupModel');
 
 
 var setupController = {
@@ -11,14 +12,14 @@ var setupController = {
     var levelsData = JSON.parse(req.body.levels);
     var classesData = JSON.parse(req.body.classes);
     var subjectsData = JSON.parse(req.body.subjects);
+    var expensesData = JSON.parse(req.body.expenses);
+    var costsData = JSON.parse(req.body.costs);
     var institutionsQuery = `INSERT INTO institutions(Institution_Name,  Institution_Logo,  Institution_Email,  Institution_Phone,  Institution_wtsp) VALUES(?,?,?,?,?)`;
     var usersQuery = `INSERT INTO users(User_Name, User_Image, User_Email, User_Phone) VALUES(?,?,?,?)`;
     var academicQuery = `INSERT INTO academicyear(AY_Label, AY_Satrtdate, AY_EndDate, Institution_ID) VALUES(?,?,?,?)`;
     var levelsQuery = `INSERT INTO levels(Level_Label, AY_ID) VALUES(?,?)`;
     var classesQuery = `INSERT INTO classes(Level_ID, Classe_Label, AY_ID) VALUES(?,?,?)`;
-    var levelSubjectsQuery = `INSERT INTO levelsubjects(Level_ID, Subject_ID, AY_ID) VALUES(?,?,?)`;
-    var subjectsQuery = `IF NOT EXISTS (SELECT * FROM subjects WHERE Subject_Label = ?) Begin INSERT INTO subjects(Subject_Label) VALUES(?) End`;
-
+   
     // execute the insert statment
     connection.query(institutionsQuery, [institutionsData.school, institutionsData.logo,institutionsData.email,institutionsData.phone,institutionsData.whatsapp], (err, institutionResult, fields) => {
       if (err) {
@@ -40,7 +41,7 @@ var setupController = {
             } else 
             {
                console.log('User Id:' + userResult.insertId);
-               connection.query(academicQuery, [academicData.year,academicData.start,academicData.end,institutionResult.insertId], (err, academicResult, fields) => {
+               connection.query(academicQuery, [academicData.year,academicData.start,academicData.end,institutionResult.insertId],async (err, academicResult, fields) => {
                 if (err) {
                   console.log(err);
                     res.json({
@@ -51,24 +52,21 @@ var setupController = {
                 } else 
                 {
                    console.log('Academic Id:' + academicResult.insertId);
+                   console.log(levelsData.levelName);
+                   for (var j = expensesData.expenseName.length - 1; j >= 0; j--) {   
+                      var expenses = await setupModel.saveExpenses(expensesData.expenseName[j],expensesData.expenseTime[j],academicResult.insertId);
+                      console.log("Expenses",expenses);
+                    }
                    if (!Array.isArray(levelsData.levelName))
                     levelsData.levelName = [levelsData.levelName];
-                  for (var i = levelsData.levelName.length - 1; i >= 0; i--) {
-                    connection.query(levelsQuery, [levelsData.levelName[i],academicResult.insertId], (err, levelResult, fields) => {
-                    if (err) {
-                      console.log(err);
-                        res.json({
-                          errors: [{
-                          field: "Save denied",
-                          errorDesc: "Level not saved"
-                        }]});
-                    } else 
-                    {
-                       console.log('Level Id:' + levelResult.insertId);
-                        if (!Array.isArray(classesData[levelsData.levelName[i + 1]].classeName))
-                          classesData[levelsData.levelName[i + 1]].classeName = [classesData[levelsData.levelName[i + 1]].classeName];
-                        for (var j = classesData[levelsData.levelName[i + 1]].classeName.length - 1; j >= 0; j--) {
-                           connection.query(classesQuery, [levelResult.insertId,classesData[levelsData.levelName[i + 1]].classeName[j],academicResult.insertId], (err, classeResult, fields) => {
+                  for (var i = 0; i < levelsData.levelName.length; i++) {
+                    var levelResult = await setupModel.saveLevels(levelsData.levelName[i],academicResult.insertId);
+                    console.log('Level Id:' , levelResult);
+                       console.log(levelsData.levelName[i]);
+                        if (!Array.isArray(classesData[levelsData.levelName[i]].classeName))
+                          classesData[levelsData.levelName[i]].classeName = [classesData[levelsData.levelName[i]].classeName];
+                        for (var j =  0; j < classesData[levelsData.levelName[i]].classeName.length; j++) {
+                           connection.query(classesQuery, [levelResult.insertId,classesData[levelsData.levelName[i]].classeName[j],academicResult.insertId], (err, classeResult, fields) => {
                               if (err) {
                                 console.log(err);
                                   res.json({
@@ -83,9 +81,20 @@ var setupController = {
                               // get inserted id
                             });
                         }
-                    }
-                    // get inserted id
-                  });
+                        console.log("Subjects",subjectsData[levelsData.levelName[i]]);
+                        var subjectName = subjectsData[levelsData.levelName[i]];
+                        for (var j =  0; j < subjectName.length; j++) {
+                          var subjectID = await setupModel.saveSubjects(subjectName[j]);
+                          var levelsubjectResult = await setupModel.saveLevelsSubjects(levelResult.insertId,subjectID,academicResult.insertId);
+                          console.log("levelssubject",levelsubjectResult);
+                        }
+                        
+                        var costsName = costsData[levelsData.levelName[i]];
+                        for (var j =  0; j < costsName.costsName.length; j++) {
+                          var expenseID = await setupModel.findExpenseID(costsName.costsName[j],academicResult.insertId);
+                          var expenseResult = await setupModel.saveLevelExpenses(levelResult.insertId,expenseID,costsName.price[j],academicResult.insertId);
+                          console.log("Expenses",expenseResult);
+                        }
                   }
                 }
                 // get inserted id
@@ -97,6 +106,7 @@ var setupController = {
       console.log('Institution Id:' + institutionResult.insertId);
 
     });
+     res.json({msg : "ok"});
   },
 };
 
---
+++ models/setupModel.js
@@ -0,0 +1,55 @@
+var connection  = require('../lib/db');
+
+
+var setupModel = {
+  saveLevels: function(Level_Label,AY_ID) {
+     return new Promise((resolve, reject) => {
+      connection.query("INSERT INTO levels(Level_Label, AY_ID) VALUES(?,?)", [Level_Label,AY_ID], (err, levelResult, fields) => {
+       if (err) reject(err);
+        else resolve(levelResult);
+      });
+    })
+  },
+  saveSubjects: function(Subject_Label) {
+     return new Promise((resolve, reject) => {
+      connection.query("INSERT INTO subjects(Subject_Label) VALUES (?) ON DUPLICATE KEY UPDATE `Subject_Label` = `Subject_Label`; SELECT Subject_ID FROM subjects WHERE `Subject_Label` = ?", [Subject_Label,Subject_Label], (err, subjectResult, fields) => {
+       if (err) reject(err);
+        else resolve(subjectResult[0].insertId === 0 ? subjectResult[1][0].Subject_ID : subjectResult[0].OkPacket.insertId);
+      });
+    })
+  },
+  saveLevelsSubjects: function(Level_ID, Subject_ID, AY_ID) {
+     return new Promise((resolve, reject) => {
+      connection.query("INSERT INTO levelsubjects(Level_ID, Subject_ID, AY_ID) VALUES (?,?,?);", [Level_ID, Subject_ID, AY_ID], (err, subjectResult, fields) => {
+       if (err) reject(err);
+        else resolve(subjectResult);
+      });
+    })
+  },
+  saveExpenses: function(Expense_Label, Expense_PaymentMethod, AY_ID) {
+     return new Promise((resolve, reject) => {
+      connection.query("INSERT INTO expenses(Expense_Label, Expense_PaymentMethod, AY_ID) VALUES (?,?,?);", [Expense_Label, Expense_PaymentMethod, AY_ID], (err, expensesResult, fields) => {
+       if (err) reject(err);
+        else resolve(expensesResult);
+      });
+    })
+  },
+  saveLevelExpenses: function(Level_ID, Expense_ID, Expense_Cost, AY_ID) {
+     return new Promise((resolve, reject) => {
+      connection.query("INSERT INTO levelexpenses(Level_ID, Expense_ID, Expense_Cost, AY_ID) VALUES (?,?,?,?);", [Level_ID, Expense_ID, Expense_Cost, AY_ID], (err, expensesResult, fields) => {
+       if (err) reject(err);
+        else resolve(expensesResult);
+      });
+    })
+  },
+  findExpenseID: function(Expense_Label, AY_ID) {
+     return new Promise((resolve, reject) => {
+      connection.query("SELECT Expense_ID FROM expenses WHERE Expense_Label = ? AND AY_ID = ?", [Expense_Label, AY_ID], (err, expensesResult, fields) => {
+       if (err) reject(err);
+        else resolve(expensesResult[0].Expense_ID);
+      });
+    })
+  },
+};
+
+module.exports = setupModel;
