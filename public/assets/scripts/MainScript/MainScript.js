b857e1b61904499daf90a2c3622dd7052f3e458f
unknown <mohamed.amine.dichkour@gmail.com>
2020-11-23 12:52:12 +0100

    UPDATE

--- public/assets/scripts/MainScript/MainScript.js
+++ public/assets/scripts/MainScript/MainScript.js
@@ -131,7 +131,7 @@ function Convert_Fr_En($date_){
 
    }
 
-    function getOS() {
+   function getOS() {
 
 	  	  var userAgent = window.navigator.userAgent,
 	      platform = window.navigator.platform,
@@ -154,6 +154,95 @@ function Convert_Fr_En($date_){
 
   		return os;
 	}
+
+	 /**
+	 * JavaScript Client Detection
+	 * Source: http://jsfiddle.net/ChristianL/AVyND/
+	 * (C) viazenetti GmbH (Christian Ludwig)
+	 */
+
+	function osDetector(window){
+
+        var os, clientStrings;
+        var nVer = navigator.appVersion;
+        var nAgt = navigator.userAgent;
+        var unknown = '-';
+
+        // system
+        var os = unknown;
+        var clientStrings = [
+            {s:'Windows 3.11', r:/Win16/},
+            {s:'Windows 95', r:/(Windows 95|Win95|Windows_95)/},
+            {s:'Windows ME', r:/(Win 9x 4.90|Windows ME)/},
+            {s:'Windows 98', r:/(Windows 98|Win98)/},
+            {s:'Windows CE', r:/Windows CE/},
+            {s:'Windows 2000', r:/(Windows NT 5.0|Windows 2000)/},
+            {s:'Windows XP', r:/(Windows NT 5.1|Windows XP)/},
+            {s:'Windows Server 2003', r:/Windows NT 5.2/},
+            {s:'Windows Vista', r:/Windows NT 6.0/},
+            {s:'Windows 7', r:/(Windows 7|Windows NT 6.1)/},
+            {s:'Windows 8.1', r:/(Windows 8.1|Windows NT 6.3)/},
+            {s:'Windows 8', r:/(Windows 8|Windows NT 6.2)/},
+            {s:'Windows NT 4.0', r:/(Windows NT 4.0|WinNT4.0|WinNT|Windows NT)/},
+            {s:'Windows ME', r:/Windows ME/},
+            {s:'Android', r:/Android/},
+            {s:'Open BSD', r:/OpenBSD/},
+            {s:'Sun OS', r:/SunOS/},
+            {s:'Linux', r:/(Linux|X11)/},
+            {s:'iOS', r:/(iPhone|iPad|iPod)/},
+            {s:'Mac OS X', r:/Mac OS X/},
+            {s:'Mac OS', r:/(MacPPC|MacIntel|Mac_PowerPC|Macintosh)/},
+            {s:'QNX', r:/QNX/},
+            {s:'UNIX', r:/UNIX/},
+            {s:'BeOS', r:/BeOS/},
+            {s:'OS/2', r:/OS\/2/},
+            {s:'Search Bot', r:/(nuhk|Googlebot|Yammybot|Openbot|Slurp|MSNBot|Ask Jeeves\/Teoma|ia_archiver)/}
+        ];
+
+        for (var id in clientStrings) {
+            var cs = clientStrings[id];
+            if (cs.r.test(nAgt)) {
+                os = cs.s;
+                break;
+            }
+        }
+
+        var osVersion = unknown;
+
+        if (/Windows/.test(os)) {
+            osVersion = /Windows (.*)/.exec(os)[1];
+            os = 'Windows';
+
+            var script = `<link rel="stylesheet" href="assets/styles/MainStyle/MainStyle_ScrollBar.css">`;
+			$('head').append(script);
+
+        }
+
+        switch (os) {
+            case 'Mac OS X':
+                osVersion = /Mac OS X (10[\.\_\d]+)/.exec(nAgt)[1];
+                break;
+
+            case 'Android':
+                osVersion = /Android ([\.\_\d]+)/.exec(nAgt)[1];
+                break;
+
+            case 'iOS':
+                osVersion = /OS (\d+)_(\d+)_?(\d+)?/.exec(nVer);
+                osVersion = osVersion[1] + '.' + osVersion[2] + '.' + (osVersion[3] | 0);
+                break;
+        }
+
+        window.jscd = {
+        	os: os,
+        	osVersion: osVersion,
+    	};
+
+    	console.log(jscd.os + ' ' + jscd.osVersion);
+        
+    }
+
+	/***********************************************/
 
 /* End Helpers _________________________________*/
 
@@ -222,7 +311,11 @@ $(document).ready(function(){
 	/***************************************************
 
 
+/* osDetector _________________________________*/
 
+	   osDetector(window);
+
+	/* End osDetector _________________________________*/
 
 	/* End Chart.js ___________________________________________*/
 
